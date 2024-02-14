6a58afbecbfced26cbc75c408ec5a7038cea9cf6
Resmerita Cosmin <vasile-cosmin.resmerita@student.tuiasi.ro>
2024-02-14 19:09:10 +0200

    first commit

---
+++ EcoConnect/src/main/java/com/example/ecoconnect/EcoConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecoconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoConnectApplication.class, args);
+    }
+
+}
---
+++ EcoConnect/src/test/java/com/example/ecoconnect/EcoConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecoconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
